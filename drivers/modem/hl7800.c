1ffe287472036ef6efb8e4cb527c861adb6df4af
Ryan Erickson <ryan.erickson@lairdconnect.com>
2020-06-29 12:22:14 -0500

    drivers: modem: hl7800: add API for IMEI and fw ver
    Add APIs for retrieving IMEI and firmware version from HL7800.
    
    Signed-off-by: Ryan Erickson <ryan.erickson@lairdconnect.com>

--- drivers/modem/hl7800.c
+++ drivers/modem/hl7800.c
@@ -1429,6 +1429,16 @@ char *mdm_hl7800_get_sn(void)
 	return ictx.mdm_sn;
 }
 
+char *mdm_hl7800_get_imei(void)
+{
+	return ictx.mdm_imei;
+}
+
+char *mdm_hl7800_get_fw_version(void)
+{
+	return ictx.mdm_revision;
+}
+
 /* Handler: +CGCONTRDP: <cid>,<bearer_id>,<apn>,<local_addr and subnet_mask>,
 *			<gw_addr>,<DNS_prim_addr>,<DNS_sec_addr> */
 static bool on_cmd_atcmdinfo_ipaddr(struct net_buf **buf, uint16_t len)
