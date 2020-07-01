1e2a8513c54eba5b0204bb532bca99879c1132a8
ahedin <andrew.hedin@lairdconnect.com>
2020-07-01 14:33:51 -0500

    subsys: net: lib: lwm2m:  Update LwM2M generic, pressure, and humidity objects
    Bug 16647 - IPSO object structure changed in Zephyr 2.x

---
+++ subsys/net/lib/lwm2m/lwm2m_resource_ids.h
@@ -0,0 +1,30 @@
+/**
+ * @file lwm2m_resource_ids.h
+ * @brief
+ *
+ * Copyright (c) 2020 Laird Connectivity
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __LWM2M_RESOURCE_IDS__
+#define __LWM2M_RESOURCE_IDS__
+
+/* clang-format off */
+#define SENSOR_VALUE_RID                  5700
+#define UNITS_RID                         5701
+#define MIN_MEASURED_VALUE_RID            5601
+#define MAX_MEASURED_VALUE_RID            5602
+#define MIN_RANGE_VALUE_RID               5603
+#define MAX_RANGE_VALUE_RID               5604
+#define RESET_MIN_MAX_MEASURED_VALUES_RID 5605
+#define TIMESTAMP_RID                     5518
+
+
+#define TEMPERATURE_OBJECT_RID   3303
+#define HUMIDITY_OBJECT_RID      3304
+#define ACCELEROMETER_OBJECT_RID 3313
+#define PRESSURE_OBJECT_RID      3323
+#define DOOR_OBJECT_RID          10351
+/* clang-format on */
+
+#endif /* __LWM2M_RESOURCE_IDS__ */
