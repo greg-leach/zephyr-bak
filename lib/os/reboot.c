238d3455bb1a6f4e545bfa879bc5f7efab00e547
Jamie McCrae <jamie.mccrae@lairdconnect.com>
2021-10-12 16:09:10 +0100

    Bug #20197 Add reboot notification

--- lib/os/reboot.c
+++ lib/os/reboot.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015 Wind River Systems, Inc.
+ * Copyright (c) 2021 Laird Connectivity
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -10,8 +11,16 @@
 extern void sys_arch_reboot(int type);
 extern void sys_clock_disable(void);
 
+__weak void sys_reboot_notification(int type)
+{
+        return;
+}
+
 void sys_reboot(int type)
 {
+	/* Notify application of pending reboot */
+	sys_reboot_notification(type);
+
 	(void)irq_lock();
 #ifdef CONFIG_SYS_CLOCK_EXISTS
 	sys_clock_disable();
